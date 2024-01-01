fba3097af9f111160fe6534c80acbcddde81fa39
Daniea1 <112690693+Daniea1@users.noreply.github.com>
2024-01-01 23:10:01 +0100

    fixed import of logo + cleanup and other small fixes

--- src/Components/ContactFormular.tsx
+++ src/Components/ContactFormular.tsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 import React from "react";
 
 const CTA = () => (
-    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col ${customColor.cardsBackground} rounded-[20px] box-shadow`}>
+    <section id="Kontakt" className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col ${customColor.cardsBackground} rounded-[20px] box-shadow`}>
         <div className="flex-1 flex flex-col">
             <h2 className={styles.heading2}>Kontakt mig</h2>
             <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
