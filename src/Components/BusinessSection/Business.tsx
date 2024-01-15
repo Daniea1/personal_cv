92b5f9b464edbc642081acfa358111fa963dd611
Daniea1 <112690693+Daniea1@users.noreply.github.com>
2024-01-15 11:02:26 +0100

    send e-mail button scrolls down to the contact component now

--- src/Components/BusinessSection/Business.tsx
+++ src/Components/BusinessSection/Business.tsx
@@ -1,29 +1,40 @@
 import React from 'react';
 import {reasons} from "../infoArray";
-import styles, {layout} from "../../style";
-import Button from "./Button";
+import styles, {customColor, layout} from "../../style";
 import FeatureCard from "./FeatureCard";
 
 const Business = () => {
+
+    const onClick1 = () => {
+        window.location.href = "#Kontakt";
+    };
+
     return (
         <section id = "" className = {`${layout.section}`} >
             <div className = {`${layout.sectionInfo}`} >
                 <h2 className = {`${styles.heading2}`} >
                     3 årsager til, <br className = "sm:block hidden" /> at du bør ansætte mig.
                 </h2 >
-                <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+                <p className = {`${styles.paragraph} max-w-[470px] my-5`} >
                     Jeg ser frem til at kunne bringe mine kompetencer og engagement
                     til dit team og bidrage til virksomhedens succes.
                 </p >
-                <Button styles="mt-10"/>
+                <button
+                    type = "button"
+                    className = {`py-2 px-3 ${customColor.buttonGradientBlue}
+                    font-poppins font-medium text-[18px] text-white
+                    outline-none rounded-xl ${styles}`}
+                    onClick={onClick1}>
+                    Send e-mail
+                </button >
             </div >
 
-            <div className={`${layout.sectionImg} flex-col`}>
-                {reasons.map((feature, index) =>(
-                    <FeatureCard key={feature.id}{...feature} index={index}/>
-                    ))}
+            <div className = {`${layout.sectionImg} flex-col`} >
+                {reasons.map((feature, index) => (
+                    <FeatureCard key = {feature.id}{...feature} index = {index} />
+                ))}
 
-            </div>
+            </div >
         </section >
     )
 };
