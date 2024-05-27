c75c09f0765795f7e66e83c61c10c5f17cfb9020
Daniea1 <112690693+Daniea1@users.noreply.github.com>
2024-05-27 18:08:05 +0200

    Re-design of navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,8 +1,7 @@
 import React, {useEffect, useState} from 'react';
-import {close, closeDark, menu, menuDark} from '../assets';
+import {close, closeDark, DJLogo, DJLogoDark, menu, menuDark} from "../assets";
 import {navLinks} from "./infoArray";
 import ThemeSwitcher from "./ThemeSwitcher";
-import {customColor} from "../style";
 
 const Navbar = () => {
     const [toggle, setToggle] = useState(false);
@@ -10,76 +9,108 @@ const Navbar = () => {
 
     useEffect(() => {
         window.addEventListener("scroll", () => {
+            // if (window.scrollY > 1 && window.scrollY < 2778) {
             if (window.scrollY > 1 ) {
-                setIsBlurry(true);
+                setIsBlurry(true)
             } else {
-                setIsBlurry(false);
+                setIsBlurry(false)
             }
-        });
+        })
     }, []);
 
     return (
-        // Logo in navbar
-        <nav className={`fixed top-5 z-10 flex py-0 max-h-[65px] w-fit xl:px-[20px] l:px-[20px] md:px-[20px] px-[20px] rounded-full
-             ${isBlurry ? 'bg-white backdrop-filter backdrop-blur-lg bg-opacity-5' : ''}`}>
 
-            {/* The responsive menu large screen */}
-            <ul className="list-none sm:flex hidden justify-center items-center flex-1">
+        //logo in navbar
+        <nav className={`fixed top-0 z-10 flex h-72 max-h-[75px] w-full dark:bg-white shadow-lg
+        ${isBlurry ? 'bg-white backdrop-filter backdrop-blur-lg bg-opacity-5 w-full' : ''}
+         px-[20px]`}>
+
+            {/*The responsive menu large screen*/}
+            <ul className = "list-none, sm:flex hidden justify-center items-center flex-1" >
+                <div className="xl:mr-96 lg:mr-72 md:mr-12 sm:mr-8">
+                    <div className = "block dark:hidden" >
+                        <img src = {DJLogo} alt = "DJLogo" className = "sm:w-[200px] lg:w-[250px] h-[62px]" />
+                    </div >
+                    <div className = "hidden dark:block" >
+                        <img src = {DJLogoDark} alt = "DJLogo" className = "sm:w-[200px] lg:w-[250px] h-[62px]" />
+                    </div >
+                </div>
                 {navLinks.map((nav) => (
-                    <li key={nav.id}
-                        className={`font-poppins font-normal cursor-pointer rounded-lg
-                         text-[16px] px-2 mr-10 text-white dark:text-black`}>
-                        <a href={`#${nav.id}`}
+                    <li key = {nav.id}
+                        className = {`font-poppins font-normal cursor-pointer text-[16px]
+                         px-2 md:mr-10 sm:mr-0 text-white dark:text-black`} >
+                        <a href = {`#${nav.id}`}
                            className="hover:border-b-2 hover:border-white dark:hover:border-black">
+
                             {nav.title}
-                        </a>
-                    </li>
+                        </a >
+
+                    </li >
                 ))}
                 <ThemeSwitcher />
-            </ul>
+            </ul >
 
-            {/* The responsive menu phone */}
-            <div className="sm:hidden flex flex-1 justify-end items-center">
-                <div className="block dark:hidden">
-                    <img
-                        src={toggle ? close : menu}
-                        alt="menu"
-                        className="w-[28px] h-[28px] object-contain mr-2"
-                        onClick={() => setToggle((prev) => !prev)}
-                    />
-                </div>
 
-                <div className="hidden dark:block">
+            {/*The responsive menu phone*/}
+            <div className = "sm:hidden flex flex-1 justify-end items-center" >
+                <div className = "block dark:hidden" >
                     <img
-                        src={toggle ? closeDark : menuDark}
-                        alt="menu"
-                        className="w-[28px] h-[28px] object-contain mr-2"
-                        onClick={() => setToggle((prev) => !prev)}
+                        src = {toggle ? close : menu}
+                        alt = "menu"
+                        className = "w-[28px] h-[28px] object-contain mr-2"
+                        onClick = {() => setToggle((prev) => !prev)}
                     />
-                </div>
+                </div >
+
+                <div className = "hidden dark:block" >
+                    <img
+                        src = {toggle ? closeDark : menuDark}
+                        alt = "menu"
+                        className = "w-[28px] h-[28px] object-contain mr-2"
+                        onClick = {() => setToggle((prev) => !prev)}
+                    />
+                </div >
 
                 <div
-                    className={`${toggle ? 'flex' : 'hidden'} p-6 ${customColor.gradientGrey}
-                     absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+                    className = {`${toggle ? 'flex' : 'hidden'} p-6 dark:bg-white bg-black
+                     absolute top-0 right-0 w-screen min-h-screen`} >
 
-                    <ul className="list-none flex flex-col justify-end items-center flex-1">
+                    <ul className = "flex flex-col items-center flex-1" >
+
+                        <div className = "block dark:hidden mb-12" >
+                            <img
+                                src = {toggle ? close : menu}
+                                alt = "menu"
+                                className = "w-[28px] h-[28px] object-contain"
+                                onClick = {() => setToggle((prev) => !prev)}
+                            />
+                        </div >
+
+                        <div className = "hidden dark:block mb-6" >
+                            <img
+                                src = {toggle ? closeDark : menuDark}
+                                alt = "menu"
+                                className = "w-[28px] h-[28px] object-contain"
+                                onClick = {() => setToggle((prev) => !prev)}
+                            />
+                        </div >
+
                         {navLinks.map((nav, index) => (
-                            <li key={nav.id}
-                                className={`font-poppins font-normal cursor-pointer
-                                dark:hover:bg-gray-200 rounded-lg text-[16px] px-2 
-                                ${index === navLinks.length - 1 ? `mr-0` : `mb-4`} text-white dark:text-black`}>
-                                <a href={`#${nav.id}`} onClick={() => setToggle((prev) => !prev)}
-                                   className="hover:border-b-2 hover:border-white dark:hover:border-black">
+                            <li key = {nav.id}
+                                className = {`font-normal cursor-pointer text-[32px] text-white dark:text-black
+                                 px-2 mb-6 hover:border-b-2 hover:border-white dark:hover:border-black`} >
+                                <a href = {`#${nav.id}`} onClick = {() => setToggle((prev) => !prev)} >
                                     {nav.title}
-                                </a>
-                            </li>
+                                </a >
+
+                            </li >
                         ))}
-                    </ul>
-                </div>
+                    </ul >
+                </div >
                 <ThemeSwitcher />
-            </div>
-        </nav>
-    );
+            </div >
+        </nav >
+    )
 };
 
 export default Navbar;
