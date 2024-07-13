4e34a2be2a113c308f8abdbf0f660e02d4134c7d
Daniea1 <112690693+Daniea1@users.noreply.github.com>
2024-07-13 18:52:50 +0200

    Færdigheder centered properly

--- src/Components/CompetenceSection/Accordion.tsx
+++ src/Components/CompetenceSection/Accordion.tsx
@@ -8,7 +8,8 @@ const Accordion = () => {
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             {accordionData.map((item, index) => (
                 <div key={index} className="col-span-1">
-                    <div className={`px-5 py-6 rounded-[20px] w-[75px] h-[75px] lg:px-5 lg:py-12 lg:w-[135px] lg:h-[135px] grid justify-items-center`}>
+                    <div className={`lg:px-5 lg:py-12 px-0 py-0 rounded-[20px] w-[75px] h-[75px]
+                     lg:w-[135px] lg:h-[135px] grid justify-items-center`}>
                         <motion.div whileHover={{ scale: 1.5 }}>
                             <div className="flex items-center justify-center cursor-pointer">
                                 <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter}`}>
